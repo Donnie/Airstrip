f065a04589e8ce25132d1acea0de06cc67d74adc
Donnie Ashok <donnieashok@gmail.com>
2020-10-09 11:34:49 +0200

    v0.3.3 - Stand with optional month
    * v0.3.3 - Clean Convo
    
    * v0.3.3 - Stand with optional month

--- stand.go
+++ stand.go
@@ -3,15 +3,29 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
+	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 	"gorm.io/gorm"
 )
 
 func (st *State) handleStand(m *tb.Message) {
-	// find out account
+	payload := strings.Fields(m.Payload)
+	var acc string
+	var mon time.Time
+
+	if len(payload) > 0 {
+		acc = payload[0]
+	}
+
+	if len(payload) > 1 {
+		layout := "Jan2006"
+		mon, _ = time.Parse(layout, payload[1]+payload[2])
+	}
+
 	var account Account
-	err := st.Orm.Where("name = ?", m.Payload).
+	err := st.Orm.Where("name = ?", acc).
 		Where("user_id = ?", m.Sender.ID).
 		First(&account).Error
 
@@ -20,20 +34,29 @@ func (st *State) handleStand(m *tb.Message) {
 		return
 	}
 
-	amount := getStand(st.Orm, account.ID)
-	output := fmt.Sprintf("*Current Standing*: `%.2f EUR`", amount)
+	amount := getStand(st.Orm, account.ID, mon)
+	output := fmt.Sprintf("*Standing*: `%.2f EUR`", amount)
 	st.Bot.Send(m.Sender, output, tb.ModeMarkdown)
 }
 
-func getStand(db *gorm.DB, acc uint) float64 {
+func getStand(db *gorm.DB, acc uint, mon time.Time) float64 {
 	var res struct {
 		Sum float64
 	}
-	db.Raw(`SELECT SUM(
+
+	query := fmt.Sprintf(`SELECT SUM(
 		CASE 
-		WHEN account_in_id = ? THEN amount * 1 
-		WHEN account_out_id = ? THEN amount * -1 
+		WHEN account_in_id = %d THEN amount * 1 
+		WHEN account_out_id = %d THEN amount * -1 
 		END
-	)/100 as sum FROM records WHERE mandate = false`, acc, acc).Scan(&res)
+	)/100 as sum
+	FROM records
+	WHERE mandate = false`, acc, acc)
+
+	if !mon.IsZero() {
+		query += fmt.Sprintf(` AND EXTRACT(MONTH FROM date) = %d`, int(mon.Month()))
+	}
+
+	db.Raw(query).Scan(&res)
 	return res.Sum
 }
