7138044b8c067cab2b6ce837cc88fdb7a93b0649
Donnie <donnieashok@gmail.com>
2020-11-01 15:34:15 +0100

    v0.4.5 - Update help and readme

--- bot.go
+++ bot.go
@@ -6,12 +6,12 @@ import (
 
 func (st *State) startBot() {
 	st.Bot.Handle("/start", st.handleHelp)
-	st.Bot.Handle("/help", st.handleHelp)
 	st.Bot.Handle("/cancel", st.handleCancel)
 	st.Bot.Handle("/delete", st.handleDelete)
+	st.Bot.Handle("/help", st.handleHelp)
 	st.Bot.Handle("/predict", st.handlePredict)
-	st.Bot.Handle("/recur", st.handleRecord)
 	st.Bot.Handle("/record", st.handleRecord)
+	st.Bot.Handle("/recur", st.handleRecord)
 	st.Bot.Handle("/stand", st.handleStand)
 	st.Bot.Handle("/view", st.handleView)
 
