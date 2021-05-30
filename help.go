45c7b6e5267d1a55b79d51f9a74fa596ad43956b
Donnie <donnieashok@gmail.com>
2021-05-30 14:52:20 +0200

    v0.5.4 - Update help text

--- help.go
+++ help.go
@@ -13,8 +13,13 @@ func (st *State) handleHelp(m *tb.Message) {
 		"\n/recur - Record an income or a charge" +
 		"\n/cancel - Cancel an ongoing record or recur process" +
 		"\n/delete - Delete any record or recur" +
+		"\n/predict - Get a prediction of your financial standing for next 12 months" +
 		"\n/predict Jan 2025 - Get a prediction of your financial standing" +
+		"\n/view - Get a list of records pertaining to the current month" +
 		"\n/view Jan 2025 - Get a list of records pertaining to the month" +
+		"\n/savings - Get savings per month from last 12 months" +
+		"\n/savings Jan 2025 - Get savings per month beginning from a certain month" +
+		"\n/stand - Get a current status of all accounts" +
 		"\n/stand Account - Get a current standing of any account" +
 		"\n/stand Account Mar 2022 - Get a month wise total effect on any account" +
 		"\n/help - To see this list again"
