f1a45dcd0f319b2daf2f60bc55a3eea09b68f754
Donnie Ashok <git@donnie.in>
2022-09-21 23:05:52 +0200

    v0.6.3 - Fix savings (#63)

--- savings.go
+++ savings.go
@@ -14,21 +14,22 @@ func (st *State) handleSavings(m *tb.Message) {
 	}
 
 	savings := st.Analyse(past, m.Sender.ID)
-	st.Bot.Send(m.Sender, savings, tb.ModeMarkdownV2)
+	st.Bot.Send(m.Sender, savings, tb.ModeHTML)
 }
 
 // Analyse generates savings analyse for userID for the past
 func (st *State) Analyse(past time.Time, userID int64) (out string) {
 	savings := st.PastSavings(userID, past)
 	cash := st.CashTillNow(userID)
-	out += fmt.Sprintf("Assets: %d EUR\n\n", cash/100)
+	out += fmt.Sprintf("Assets: %.2f EUR\n\n", float64(cash)/100)
 
 	for _, save := range savings {
 		// show only last twelve months
 		// because of telegram message size limitation
+		dateTime := parseDate(save.Month)
 		out += fmt.Sprintf(
-			"*%s:* `%.2f EUR`\n",
-			save.Month.Format(monthFormat), (float64(save.Effect) / 100),
+			"<strong>%s:</strong> <code>%.2f EUR</code>\n",
+			dateTime.Format(monthFormat), (float64(save.Effect) / 100),
 		)
 	}
 	return
@@ -36,38 +37,41 @@ func (st *State) Analyse(past time.Time, userID int64) (out string) {
 
 // PastSavings calculates savings till a past date
 func (st *State) PastSavings(userID int64, past time.Time) (savings []Saving) {
-	st.Orm.Raw(`SELECT
-		month,
-		COALESCE((
-			SELECT
-				SUM(
-					CASE
-						WHEN aci.self AND aco.self = false THEN amount
-						WHEN aci.self = false AND aco.self THEN amount * -1
-					END
-				) as delta
-			FROM records AS r
-			LEFT JOIN accounts AS aci ON r.account_in_id = aci.id
-			LEFT JOIN accounts AS aco ON r.account_out_id = aco.id
-			WHERE 
-				r.deleted_at IS NULL
-				AND (aci.self = false OR aco.self = false)
-				AND r.date IS NOT NULL
-				AND DATE_TRUNC('month',r.date) = month
-				AND r.user_id = ?
-		), 0) as effect
-	FROM (
+	query := fmt.Sprintf(`
+		WITH RECURSIVE MonthDates(monthDate) AS (
+			SELECT '%s'
+			UNION ALL
+			SELECT date(monthDate, '+1 month')
+			FROM MonthDates
+			WHERE date(monthDate, '+1 month') < CURRENT_TIMESTAMP
+		)
 		SELECT
-			DATE_TRUNC('month', current_date) - (INTERVAL '1 month' * GENERATE_SERIES(0, months::int)) AS month
-		FROM 
-			(SELECT EXTRACT(year FROM diff) * 12 + EXTRACT(month FROM diff) AS months
-			FROM 
-				(SELECT age(
-						DATE_TRUNC('month', CURRENT_TIMESTAMP),
-						DATE_TRUNC('month', CAST(? AS TIMESTAMP)) 
-				) AS diff
-			) AS fut
-		) AS reps
-	) AS months`, userID, past).Scan(&savings)
+			endDate as month,
+			COALESCE((
+				SELECT
+					SUM(
+						CASE
+							WHEN aci.self = 1 AND aco.self = 0 THEN amount
+							WHEN aci.self = 0 AND aco.self = 1 THEN amount * -1
+						END
+					) as delta
+				FROM records AS r
+				LEFT JOIN accounts AS aci ON r.account_in_id = aci.id
+				LEFT JOIN accounts AS aco ON r.account_out_id = aco.id
+				WHERE
+					r.deleted_at IS NULL
+					AND (aci.self = 0 OR aco.self = 0)
+					AND r.date IS NOT NULL
+					AND r.date BETWEEN months.startDate AND months.endDate
+					AND r.user_id = %d
+			), 0) as effect
+		FROM (
+			SELECT date(monthDate, 'start of month') as startDate,
+				date(monthDate, '+1 month', 'start of month', '-1 day') as endDate
+			FROM MonthDates
+		) AS months
+	`, past.Format("2006-01-02"), userID)
+
+	st.Orm.Raw(query).Scan(&savings)
 	return
 }
