7c12132e78962c9d9da9eb77c0b83497f4545971
Donnie <git@donnie.in>
2023-01-01 21:34:10 +0100

    v0.6.7 - Improve

--- cancel.go
+++ cancel.go
@@ -4,15 +4,17 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+// handleCancel cancels the last conversation
 func (st *State) handleCancel(m *tb.Message) {
+	// Get the last conversation.
 	var convo Convo
-	// get last conversation
 	st.Orm.Where("user_id = ?", m.Sender.ID).First(&convo)
-	// end last conversation
+
+	// End the last conversation.
 	st.Orm.Unscoped().Where("user_id = ?", m.Sender.ID).Delete(&Convo{})
 
+	// Remove the related record.
 	if convo.ContextID != nil {
-		// remove related record
 		st.Orm.Unscoped().
 			Where(*convo.ContextID).
 			Delete(&Record{})
